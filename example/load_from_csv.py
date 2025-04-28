54a1e764525276ca8eb0f1bc924ed955544ec079
shunsock <s.tsuchiya.business@gmail.com>
2025-04-29 00:28:06 +0900

    feature: add example for invalid csv

--- example/load_from_csv.py
+++ example/load_from_csv.py
@@ -5,17 +5,17 @@ import pandas
 
 
 class User(BaseModel):
-   id: int
-   name: str
-   age: int
+    id: int
+    name: str
+    age: int
 
 
 try:
-   users: CsvLoaded[User] = CsvLoader(User).load("./users.csv")
-   print(users)
-   print(users.value[1].value.name)
+    users: CsvLoaded[User] = CsvLoader(User).load("./users.csv")
+    print(users)
+    print(users.value[1].value.name)
 except LoadingException as e:
-   print(f"Failed to load: {e}")
+    print(f"Failed to load: {e}")
 
 users_dataframe = users.to_polars(include_path_as_column=True)
 print(users_dataframe)
